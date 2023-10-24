fa18d84b8115f6b65c575da8af75fcfcc8e27dc8
Hadi-loo <m.h.babalu@gmail.com>
2023-10-24 20:54:13 +0330

    Update unit tests for Comment class

--- CA1/src/test/java/model/CommentTest.java
+++ CA1/src/test/java/model/CommentTest.java
@@ -18,62 +18,95 @@ public class CommentTest {
         comment = new Comment();
     }
 
+    public void setUpWithArgs() {
+        comment = new Comment(1, "sana.sarinavaei@gmail.com", "sana", 10, "meh");
+    }
+
     @Test
-    @DisplayName("Simple Test Comment Initialization with Id")
+    @DisplayName("comment id initialed correctly")
     public void InitialIdTest() {
-        comment.setId(1);
+        setUpWithArgs();
         assertEquals(1, comment.getId());
     }
 
     @Test
-    @DisplayName("Simple Test Comment Initialization with userEmail")
+    @DisplayName("comment email initialed correctly")
     public void InitialEmailTest() {
-        comment.setUserEmail("sana.sarinavaei@gmail.com");
+        setUpWithArgs();
         assertEquals("sana.sarinavaei@gmail.com", comment.getUserEmail());
     }
 
     @Test
-    @DisplayName("Simple Test Comment Initialization with username")
+    @DisplayName("comment username initialed correctly")
     public void InitialUsernameTest() {
-        comment.setUsername("sana");
+        setUpWithArgs();
         assertEquals("sana", comment.getUsername());
     }
 
     @Test
-    @DisplayName("Simple Test Comment Initialization with commodityId")
+    @DisplayName("comment commodityId initialed correctly")
     public void InitialCommodityIdTest() {
-        comment.setCommodityId(10);
+        setUpWithArgs();
         assertEquals(10, comment.getCommodityId());
     }
 
     @Test
-    @DisplayName("Simple Test Comment Initialization with text")
+    @DisplayName("comment text initialed correctly")
     public void InitialTextTest() {
-        comment.setText("meh");
+        setUpWithArgs();
         assertEquals("meh", comment.getText());
     }
 
     @Test
-    @DisplayName("Test Comment date not null")
+    @DisplayName("comment date should not be null")
     public void NotNullDateTest() {
         comment = new Comment(1, "sana.sarinavaei@gmail.com", "sana", 10, "meh");
         assertNotNull(comment.getDate());
     }
 
     @Test
+    @DisplayName("comment date format should be correct")
+    public void CorrectDateFormatTest() {
+        setUpWithArgs();
+        assertTrue(comment.getDate().matches("\\d{4}-\\d{2}-\\d{2} \\d{2}:\\d{2}:\\d{2}"));
+    }
+
+    @Test
     @DisplayName("Test to check the Comment date")
     public void DateTest() {
         comment = new Comment(1, "sana.sarinavaei@gmail.com", "sana", 10, "meh");
-        // expected date
+
         Date currentDate = new Date();
         SimpleDateFormat dateFormat = new SimpleDateFormat("yyyy-MM-dd");
         String expected_date = dateFormat.format(currentDate);
 
-        // actual date
         String[] commentDate = comment.getDate().split(" ");
         String actual_date = commentDate[0];
 
         assertEquals(expected_date, actual_date);
+    }
+
+    @ParameterizedTest
+    @CsvSource({
+            "sana, like",
+            "hadi, dislike"
+    })
+    @DisplayName("record added to UserVote")
+    public void recordAddedToUserVoteTest(String username, String vote) {
+        comment.addUserVote(username, vote);
+        assertTrue(comment.getUserVote().containsKey(username));
+    }
+
+    @ParameterizedTest
+    @CsvSource({
+            "sana, like",
+            "hadi, dislike"
+    })
+    @DisplayName("Test to check the size of UserVote")
+    public void UserVoteLengthTest(String username, String vote) {
+        int length = comment.getUserVote().size();
+        comment.addUserVote(username, vote);
+        assertEquals(length + 1, comment.getUserVote().size());
     }
 
     @ParameterizedTest
@@ -92,18 +125,6 @@ public class CommentTest {
         int first_dislike = comment.getDislike();
         comment.addUserVote(username, vote);
         assertEquals(first_dislike + 1, comment.getDislike());
-    }
-
-    @ParameterizedTest
-    @CsvSource({
-            "sana, like",
-            "hadi, dislike"
-    })
-    @DisplayName("Test to check the size of UserVote")
-    public void UserVoteLengthTest(String username, String vote) {
-        int length = comment.getUserVote().size();
-        comment.addUserVote(username, vote);
-        assertEquals(length + 1, comment.getUserVote().size());
     }
 
     @ParameterizedTest
