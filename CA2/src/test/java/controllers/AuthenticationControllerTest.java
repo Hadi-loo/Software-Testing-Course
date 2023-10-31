a0d9efb9df51b18d8978bd5d8b7a8ef60f9a86c4
SanaNavaei <sana.sarinavaei@gmail.com>
2023-10-31 19:04:17 +0330

    Add unit tests for AuthenticationController class

--- CA2/src/test/java/controllers/AuthenticationControllerTest.java
+++ CA2/src/test/java/controllers/AuthenticationControllerTest.java
@@ -2,6 +2,7 @@ package controllers;
 
 import exceptions.IncorrectPassword;
 import exceptions.NotExistentUser;
+import exceptions.UsernameAlreadyTaken;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
@@ -10,10 +11,11 @@ import org.springframework.http.ResponseEntity;
 import service.Baloot;
 
 import java.util.HashMap;
-import java.util.List;
 import java.util.Map;
 
+import static defines.Errors.INCORRECT_PASSWORD;
 import static defines.Errors.NOT_EXISTENT_USER;
+import static defines.Errors.USERNAME_ALREADY_TAKEN;
 import static org.junit.jupiter.api.Assertions.*;
 import static org.mockito.Mockito.*;
 
@@ -31,23 +33,29 @@ public class AuthenticationControllerTest {
 
     @Test
     @DisplayName("HTTP status should be correct when login is successful")
-    public void testLoginStatusOk() {
+    public void testLoginStatusOk() throws NotExistentUser, IncorrectPassword {
         Map<String, String> input = new HashMap<>();
         input.put("username", "hadi");
         input.put("password", "1234");
+        doNothing().when(balootMock).login("hadi", "1234");
 
         ResponseEntity<String> response = authenticationController.login(input);
+
+        verify(balootMock).login("hadi", "1234");
         assertEquals(HttpStatus.OK, response.getStatusCode());
     }
 
     @Test
     @DisplayName("Response body should be correct when login is successful")
-    public void testLoginResponseBody() {
+    public void testLoginResponseBody() throws NotExistentUser, IncorrectPassword {
         Map<String, String> input = new HashMap<>();
         input.put("username", "hadi");
         input.put("password", "1234");
+        doNothing().when(balootMock).login("hadi", "1234");
 
         ResponseEntity<String> response = authenticationController.login(input);
+
+        verify(balootMock).login("hadi", "1234");
         assertEquals("login successfully!", response.getBody());
     }
 
@@ -57,21 +65,121 @@ public class AuthenticationControllerTest {
         Map<String, String> input = new HashMap<>();
         input.put("username", "hadi");
         input.put("password", "1234");
+        doThrow(new NotExistentUser()).when(balootMock).login("hadi", "1234");
 
-        doThrow(new NotExistentUser()).when(balootMock).login(argThat((List args) -> args.get(0).equals("hadi")));
         ResponseEntity<String> response = authenticationController.login(input);
+
+        verify(balootMock).login("hadi", "1234");
         assertEquals(HttpStatus.NOT_FOUND, response.getStatusCode());
     }
 
     @Test
     @DisplayName("Error message should be correct when user not exist")
-    public void testLoginErrorNotFound() throws NotExistentUser, IncorrectPassword {
+    public void testLoginErrorBodyMessageUserNotFound() throws NotExistentUser, IncorrectPassword {
         Map<String, String> input = new HashMap<>();
         input.put("username", "hadi");
         input.put("password", "1234");
+        doThrow(new NotExistentUser()).when(balootMock).login("hadi", "1234");
 
-        doThrow(new NotExistentUser()).when(balootMock).login(argThat((List args) -> args.get(0).equals("hadi")));
         ResponseEntity<String> response = authenticationController.login(input);
+
+        verify(balootMock).login("hadi", "1234");
         assertEquals(NOT_EXISTENT_USER, response.getBody());
     }
+
+    @Test
+    @DisplayName("Http status should be Unauthorized when password is incorrect")
+    public void testLoginErrorPasswordIncorrect() throws NotExistentUser, IncorrectPassword {
+        Map<String, String> input = new HashMap<>();
+        input.put("username", "hadi");
+        input.put("password", "1234");
+        doThrow(new IncorrectPassword()).when(balootMock).login("hadi", "1234");
+
+        ResponseEntity<String> response = authenticationController.login(input);
+
+        verify(balootMock).login("hadi", "1234");
+        assertEquals(HttpStatus.UNAUTHORIZED, response.getStatusCode());
+    }
+    @Test
+    @DisplayName("Error message should be correct when password is incorrect")
+    public void testLoginErrorBodyMessagePasswordIncorrect() throws NotExistentUser, IncorrectPassword {
+        Map<String, String> input = new HashMap<>();
+        input.put("username", "hadi");
+        input.put("password", "1234");
+        doThrow(new IncorrectPassword()).when(balootMock).login("hadi", "1234");
+
+        ResponseEntity<String> response = authenticationController.login(input);
+
+        verify(balootMock).login("hadi", "1234");
+        assertEquals(INCORRECT_PASSWORD, response.getBody());
+    }
+
+    @Test
+    @DisplayName("HTTP status should be correct when signup is successful")
+    public void testSignupStatusOk() throws UsernameAlreadyTaken {
+        Map<String, String> input = new HashMap<>();
+        input.put("address", "Sari, Iran");
+        input.put("birthDate", "2001-11-21");
+        input.put("email", "sana.sarinavaei@gmail.com");
+        input.put("username", "sana");
+        input.put("password", "1234");
+        doNothing().when(balootMock).addUser(any());
+
+        ResponseEntity<String> response = authenticationController.signup(input);
+
+        verify(balootMock).addUser(any());
+        assertEquals(HttpStatus.OK, response.getStatusCode());
+    }
+
+    @Test
+    @DisplayName("Response body should be correct when signup is successful")
+    public void testSignupResponseBody() throws UsernameAlreadyTaken {
+        Map<String, String> input = new HashMap<>();
+        input.put("address", "Sari, Iran");
+        input.put("birthDate", "2001-11-21");
+        input.put("email", "sana.sarinavaei@gmail.com");
+        input.put("username", "sana");
+        input.put("password", "1234");
+        doNothing().when(balootMock).addUser(any());
+
+        ResponseEntity<String> response = authenticationController.signup(input);
+
+        verify(balootMock).addUser(any());
+        assertEquals("signup successfully!", response.getBody());
+    }
+
+    @Test
+    @DisplayName("HTTP status should be bad request when user exists")
+    public void testSignupErrorUserExists() throws UsernameAlreadyTaken {
+        Map<String, String> input = new HashMap<>();
+        input.put("address", "Sari, Iran");
+        input.put("birthDate", "2001-11-21");
+        input.put("email", "sana.sarinavaei@gmail.com");
+        input.put("username", "sana");
+        input.put("password", "1234");
+        doThrow(new UsernameAlreadyTaken()).when(balootMock).addUser(argThat(user -> user.getUsername().equals("sana")));
+
+        ResponseEntity<String> response = authenticationController.signup(input);
+
+        verify(balootMock).addUser(argThat(user -> user.getUsername().equals("sana")));
+        assertEquals(HttpStatus.BAD_REQUEST, response.getStatusCode());
+    }
+
+    @Test
+    @DisplayName("Error message should be correct when user exists")
+    public void testSignupBodyMessageUserExists() throws UsernameAlreadyTaken {
+        Map<String, String> input = new HashMap<>();
+        input.put("address", "Sari, Iran");
+        input.put("birthDate", "2001-11-21");
+        input.put("email", "sana.sarinavaei@gmail.com");
+        input.put("username", "sana");
+        input.put("password", "1234");
+        doThrow(new UsernameAlreadyTaken()).when(balootMock).addUser(argThat(user -> user.getUsername().equals("sana")));
+
+        ResponseEntity<String> response = authenticationController.signup(input);
+
+        verify(balootMock).addUser(argThat(user -> user.getUsername().equals("sana")));
+        assertEquals(USERNAME_ALREADY_TAKEN, response.getBody());
+    }
+
 }
