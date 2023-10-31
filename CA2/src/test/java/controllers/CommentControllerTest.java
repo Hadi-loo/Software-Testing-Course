bf3e89c10285d719d7a6d4c964e007105173c637
Hadi-loo <m.h.babalu@gmail.com>
2023-10-31 21:55:33 +0330

    Refactor unit tests of CommentController class

--- CA2/src/test/java/controllers/CommentControllerTest.java
+++ CA2/src/test/java/controllers/CommentControllerTest.java
@@ -18,11 +18,14 @@ import static org.mockito.Mockito.*;
 
 public class CommentControllerTest {
 
-    CommentController commentController;
+    private CommentController commentController;
+    private Baloot balootMock;
 
     @BeforeEach
     public void setUp() {
         commentController = new CommentController();
+        balootMock = mock(Baloot.class);
+        commentController.setBaloot(balootMock);
     }
 
     @Test
@@ -30,14 +33,12 @@ public class CommentControllerTest {
     void testLikeCommentStatusOk() throws NotExistentComment {
         Map<String, String> input = new HashMap<>();
         input.put("username", "hadi");
-
-        Baloot balootMock = mock(Baloot.class);
         Comment commentMock = mock(Comment.class);
         when(balootMock.getCommentById(1)).thenReturn(commentMock);
         doNothing().when(commentMock).addUserVote("hadi", "like");
 
-        commentController.setBaloot(balootMock);
         ResponseEntity<String> response = commentController.likeComment("1", input);
+
         verify(balootMock).getCommentById(1);
         verify(commentMock).addUserVote("hadi", "like");
         assertEquals(HttpStatus.OK, response.getStatusCode());
@@ -48,14 +49,12 @@ public class CommentControllerTest {
     void testLikeCommentResponseBody() throws NotExistentComment {
         Map<String, String> input = new HashMap<>();
         input.put("username", "hadi");
-
-        Baloot balootMock = mock(Baloot.class);
         Comment commentMock = mock(Comment.class);
         when(balootMock.getCommentById(2)).thenReturn(commentMock);
         doNothing().when(commentMock).addUserVote("hadi", "like");
 
-        commentController.setBaloot(balootMock);
         ResponseEntity<String> response = commentController.likeComment("2", input);
+
         verify(balootMock).getCommentById(2);
         verify(commentMock).addUserVote("hadi", "like");
         assertEquals("The comment was successfully liked!", response.getBody());
@@ -66,12 +65,10 @@ public class CommentControllerTest {
     void testLikeCommentErrorNotFound() throws NotExistentComment {
         Map<String, String> input = new HashMap<>();
         input.put("username", "hadi");
+        when(balootMock.getCommentById(3)).thenThrow(new NotExistentComment());
 
-        Baloot balootMock = mock(Baloot.class);
-        when(balootMock.getCommentById(3)).thenThrow(NotExistentComment.class);
-
-        commentController.setBaloot(balootMock);
         ResponseEntity<String> response = commentController.likeComment("3", input);
+
         verify(balootMock).getCommentById(3);
         assertEquals(HttpStatus.NOT_FOUND, response.getStatusCode());
     }
@@ -81,14 +78,10 @@ public class CommentControllerTest {
     void testLikeCommentErrorBodyMessage() throws NotExistentComment {
         Map<String, String> input = new HashMap<>();
         input.put("username", "hadi");
+        when(balootMock.getCommentById(4)).thenThrow(new NotExistentComment());
 
-        Baloot balootMock = mock(Baloot.class);
-        NotExistentComment errorMock = mock(NotExistentComment.class);
-        when(balootMock.getCommentById(4)).thenThrow(errorMock);
-        when(errorMock.getMessage()).thenReturn(NOT_EXISTENT_COMMENT);
-
-        commentController.setBaloot(balootMock);
         ResponseEntity<String> response = commentController.likeComment("4", input);
+
         verify(balootMock).getCommentById(4);
         assertEquals(NOT_EXISTENT_COMMENT, response.getBody());
     }
@@ -98,14 +91,12 @@ public class CommentControllerTest {
     void testDislikeCommentStatusOk() throws NotExistentComment {
         Map<String, String> input = new HashMap<>();
         input.put("username", "sana");
-
-        Baloot balootMock = mock(Baloot.class);
         Comment commentMock = mock(Comment.class);
         when(balootMock.getCommentById(1)).thenReturn(commentMock);
         doNothing().when(commentMock).addUserVote("sana", "dislike");
 
-        commentController.setBaloot(balootMock);
         ResponseEntity<String> response = commentController.dislikeComment("1", input);
+
         verify(balootMock).getCommentById(1);
         verify(commentMock).addUserVote("sana", "dislike");
         assertEquals(HttpStatus.OK, response.getStatusCode());
@@ -116,14 +107,12 @@ public class CommentControllerTest {
     void testDislikeCommentResponseBody() throws NotExistentComment {
         Map<String, String> input = new HashMap<>();
         input.put("username", "sana");
-
-        Baloot balootMock = mock(Baloot.class);
         Comment commentMock = mock(Comment.class);
         when(balootMock.getCommentById(2)).thenReturn(commentMock);
         doNothing().when(commentMock).addUserVote("sana", "dislike");
 
-        commentController.setBaloot(balootMock);
         ResponseEntity<String> response = commentController.dislikeComment("2", input);
+
         verify(balootMock).getCommentById(2);
         verify(commentMock).addUserVote("sana", "dislike");
         assertEquals("The comment was successfully disliked!", response.getBody());
@@ -134,12 +123,10 @@ public class CommentControllerTest {
     void testDislikeCommentErrorNotFound() throws NotExistentComment {
         Map<String, String> input = new HashMap<>();
         input.put("username", "sana");
+        when(balootMock.getCommentById(3)).thenThrow(new NotExistentComment());
 
-        Baloot balootMock = mock(Baloot.class);
-        when(balootMock.getCommentById(3)).thenThrow(NotExistentComment.class);
-
-        commentController.setBaloot(balootMock);
         ResponseEntity<String> response = commentController.dislikeComment("3", input);
+
         verify(balootMock).getCommentById(3);
         assertEquals(HttpStatus.NOT_FOUND, response.getStatusCode());
     }
@@ -149,14 +136,10 @@ public class CommentControllerTest {
     void testDislikeCommentErrorBodyMessage() throws NotExistentComment {
         Map<String, String> input = new HashMap<>();
         input.put("username", "sana");
+        when(balootMock.getCommentById(4)).thenThrow(new NotExistentComment());
 
-        Baloot balootMock = mock(Baloot.class);
-        NotExistentComment errorMock = mock(NotExistentComment.class);
-        when(balootMock.getCommentById(4)).thenThrow(errorMock);
-        when(errorMock.getMessage()).thenReturn(NOT_EXISTENT_COMMENT);
-
-        commentController.setBaloot(balootMock);
         ResponseEntity<String> response = commentController.dislikeComment("4", input);
+
         verify(balootMock).getCommentById(4);
         assertEquals(NOT_EXISTENT_COMMENT, response.getBody());
     }
