43a71d8d5adba24923879238c5b97cbe86138be1
Hadi-loo <m.h.babalu@gmail.com>
2023-11-11 01:02:44 +0330

    Added some tests for Engine class
    Also refactored previous tests a little bit

--- CA3/src/test/java/domain/EngineTest.java
+++ CA3/src/test/java/domain/EngineTest.java
@@ -4,8 +4,6 @@ import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
 import org.junit.jupiter.api.BeforeEach;
 
-import java.util.ArrayList;
-
 import static org.junit.jupiter.api.Assertions.*;
 
 public class EngineTest {
@@ -29,7 +27,7 @@ public class EngineTest {
     public void testAverageQuantityIsZeroWithNoOrder() throws Exception {
         int customer_id = 5;
         int averageOrderQuantityByCustomer = engine.getAverageOrderQuantityByCustomer(customer_id);
-        assertEquals(averageOrderQuantityByCustomer, 0);
+        assertEquals(0, averageOrderQuantityByCustomer);
     }
 
     @Test
@@ -38,7 +36,7 @@ public class EngineTest {
         setUpWithArgs(order,1, 5, 10, 5);
         engine.orderHistory.add(order);
         int averageOrderQuantityByCustomer = engine.getAverageOrderQuantityByCustomer(5);
-        assertEquals(averageOrderQuantityByCustomer, 5);
+        assertEquals(5, averageOrderQuantityByCustomer );
     }
 
     @Test
@@ -50,7 +48,7 @@ public class EngineTest {
         setUpWithArgs(order2, 2, 5, 10, 10);
         engine.orderHistory.add(order2);
         int averageOrderQuantityByCustomer = engine.getAverageOrderQuantityByCustomer(5);
-        assertEquals(averageOrderQuantityByCustomer, 8);
+        assertEquals(8, averageOrderQuantityByCustomer);
     }
 
     @Test
@@ -65,7 +63,7 @@ public class EngineTest {
     @DisplayName("quantity difference should be 0 when there are no orders")
     public void testQuantityDifferenceIsZeroWithNoOrder() {
         int quantityPatternByPrice = engine.getQuantityPatternByPrice(20);
-        assertEquals(quantityPatternByPrice, 0);
+        assertEquals(0, quantityPatternByPrice);
     }
 
     @Test
@@ -74,7 +72,7 @@ public class EngineTest {
         setUpWithArgs(order, 1, 5, 10, 6);
         engine.orderHistory.add(order);
         int quantityPatternByPrice = engine.getQuantityPatternByPrice(20);
-        assertEquals(quantityPatternByPrice, 0);
+        assertEquals(0, quantityPatternByPrice);
     }
 
     @Test
@@ -86,7 +84,7 @@ public class EngineTest {
         setUpWithArgs(order2, 2, 5, 20, 10);
         engine.orderHistory.add(order2);
         int quantityPatternByPrice = engine.getQuantityPatternByPrice(30);
-        assertEquals(quantityPatternByPrice, 0);
+        assertEquals(0, quantityPatternByPrice);
     }
 
     @Test
@@ -98,6 +96,85 @@ public class EngineTest {
         setUpWithArgs(order2, 2, 5, 10, 10);
         engine.orderHistory.add(order2);
         int quantityPatternByPrice = engine.getQuantityPatternByPrice(10);
-        assertEquals(quantityPatternByPrice, 4);
+        assertEquals(4, quantityPatternByPrice);
     }
+
+    @Test
+    @DisplayName("fraudulent quantity should be 0 when the order quantity is less than or equal to the average order quantity")
+    public void testFraudulentQuantityIsZeroWithLessEqualOrderQuantity() throws Exception {
+        setUpWithArgs(order, 1, 5, 5, 6);
+        engine.orderHistory.add(order);
+        int fraudulentQuantity = engine.getCustomerFraudulentQuantity(order);
+        assertEquals(0, fraudulentQuantity);
+    }
+
+    @Test
+    @DisplayName("fraudulent quantity should be correct when the order quantity is greater than the average order quantity")
+    public void testFraudulentQuantityIsCorrectWithGreaterOrderQuantity() throws Exception {
+        setUpWithArgs(order, 1, 5, 5, 10);
+        engine.orderHistory.add(order);
+        Order order2 = new Order();
+        setUpWithArgs(order2, 2, 5, 5, 4);
+        engine.orderHistory.add(order2);
+        int fraudulentQuantity = engine.getCustomerFraudulentQuantity(order);
+        assertEquals(3, fraudulentQuantity);
+    }
+
+    @Test
+    @DisplayName("an exception should be thrown when the customer id does not exist")
+    public void testFraudulentQuantityThrowExceptionWithNonExistentCustomer() throws Exception {
+        setUpWithArgs(order, 1, 5, 5, 10);
+        Order order2 = new Order();
+        setUpWithArgs(order2, 2, 6, 5, 10);
+        engine.orderHistory.add(order2);
+        assertThrows(Exception.class, () -> engine.getCustomerFraudulentQuantity(order));
+    }
+
+    @Test
+    @DisplayName("add order should return 0 when the order already exists")
+    public void testAddOrderReturnZeroWithExistingOrder() throws Exception {
+        setUpWithArgs(order, 1, 5, 5, 10);
+        engine.orderHistory.add(order);
+        int fraudulentQuantity = engine.addOrderAndGetFraudulentQuantity(order);
+        assertEquals(0, fraudulentQuantity);
+    }
+
+    @Test
+    @DisplayName("add order should throw an exception when customer id does not exist")
+    public void testAddOrderThrowExceptionWithNonExistentCustomer() throws Exception {
+        setUpWithArgs(order, 1, 5, 5, 10);
+        Order order2 = new Order();
+        setUpWithArgs(order2, 2, 6, 5, 10);
+        engine.orderHistory.add(order2);
+        assertThrows(Exception.class, () -> engine.addOrderAndGetFraudulentQuantity(order));
+    }
+
+    @Test
+    @DisplayName("add order should return fraudulent quantity when the fraudulent quantity is not 0")
+    public void testAddOrderReturnFraudulentQuantityWithGreaterOrderQuantity() throws Exception {
+        setUpWithArgs(order, 1, 5, 5, 10);
+        Order order2 = new Order();
+        setUpWithArgs(order2, 2, 5, 5, 4);
+        engine.orderHistory.add(order2);
+        Order order3 = new Order();
+        setUpWithArgs(order3, 3, 5, 5, 10);
+        engine.orderHistory.add(order3);
+        int fraudulentQuantity = engine.addOrderAndGetFraudulentQuantity(order);
+        assertEquals(3, fraudulentQuantity);
+    }
+
+    @Test
+    @DisplayName("add order should return quantity pattern when the fraudulent quantity is 0")
+    public void testAddOrderReturnQuantityPatternWithSamePrices() throws Exception {
+        setUpWithArgs(order, 1, 5, 10, 4);
+        Order order2 = new Order();
+        setUpWithArgs(order2, 2, 5, 5, 6);
+        engine.orderHistory.add(order2);
+        Order order3 = new Order();
+        setUpWithArgs(order3, 3, 5, 10, 10);
+        engine.orderHistory.add(order3);
+        int fraudulentQuantity = engine.addOrderAndGetFraudulentQuantity(order);
+        assertEquals(4, fraudulentQuantity);
+    }
+
 }
